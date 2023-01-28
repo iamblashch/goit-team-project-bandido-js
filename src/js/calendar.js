498c6aa85ea927cf8c9833c2776ffb36edd110a9
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-01-28 14:09:40 +0200

    Refactored code
    Change color of next/previous calendar arrows

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -7,7 +7,6 @@ const API_KEY = 'ef883f39deda4d31b974aca841c64d64';
 function makeFetchByDate(selectedDate) {
     return fetch(`https://newsapi.org/v2/everything?q=cat&from=${selectedDate}&to=${selectedDate}&apiKey=${API_KEY}`)
         .then(response => {
-            // console.log(se)
             if (!response.ok) {
       throw new Error(response.status);
     }
@@ -39,7 +38,9 @@ const options = {
 
 const fp = flatpickr(dateInputEl, options);
 const fpDivEl = document.querySelector('.flatpickr-calendar');
-// const fpDivDaysEl = document.querySelector('.flatpickr-day')
-fpDivEl.style.cssText = 'background-color:#F8F8F8; box-shadow: 0px 10px 60px rgba(0, 0, 0, 0.1);border-radius: 20px';
-// fpDivDaysEl.style.cssText = 'color:red';
-console.log(fp)
+const fpPrevMonthArrowEl = document.querySelector('.flatpickr-prev-month');
+const fpNextMonthArrowEl = document.querySelector('.flatpickr-next-month');
+
+fpDivEl.style.cssText = 'background-color:#F8F8F8; color:#000000; box-shadow: 0px 10px 60px rgba(0, 0, 0, 0.1);border-radius: 20px';
+fpPrevMonthArrowEl.style.cssText = 'fill:#4440F6';
+fpNextMonthArrowEl.style.cssText = 'fill:#4440F6';
