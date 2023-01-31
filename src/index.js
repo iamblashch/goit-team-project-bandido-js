f3030ae52819c03195112535ff4bfca968c36109
https://github.com/iamblashch <bblashchuk08@gmail.com>
2023-01-31 11:05:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import './js/them-toggle'
 import './js/input-serch-fetch-news';
 import './js/calendar';
 import './js/news';
 import './js/mobile-menu';
 import './js/pagination';
-import './js/them-toggle'
 import './js/weather-section';
 import './js/category-search-list'
--- src/js/news.js
+++ src/js/news.js
@@ -23,19 +23,19 @@ const ellipsisSecondx2 = document
 const nextPagex2 = document.querySelector(`.pagination-next`);
 let page = 0;
 
-const numberPageApi = (lastBtn.textContent = 12);
+// const numberPageApi = (lastBtn.textContent = 12);
 
-// function newsAPI() {
-//   return new Promise((resolve, reject) => {
-//     axios.get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${API_KEY}`)
-//       .then(response => {
-//         resolve(response.data.results);
-//       })
-//       .catch(error => {
-//         reject(error);
-//       });
-//   });
-// }
+// // function newsAPI() {
+// //   return new Promise((resolve, reject) => {
+// //     axios.get(`https://api.nytimes.com/svc/topstories/v2/arts.json?api-key=${API_KEY}`)
+// //       .then(response => {
+// //         resolve(response.data.results);
+// //       })
+// //       .catch(error => {
+// //         reject(error);
+// //       });
+// //   });
+// // }
 function newsAPI() {
   return new Promise((resolve, reject) => {
     axios
@@ -109,98 +109,98 @@ function CreatCardNews(news) {
   sectionCard.insertAdjacentHTML('afterbegin', weather());
 }
 
-ListenerNumberPage.addEventListener(`click`, e => {
-  let currentElemNumber = document.querySelector(`.is-current`);
+// ListenerNumberPage.addEventListener(`click`, e => {
+//   let currentElemNumber = document.querySelector(`.is-current`);
 
-  if (currentElemNumber.textContent === `1`) {
-    const ellipsisSecondx2 = document
-      .querySelector(`.pagination-ellipsis_item2`)
-      .classList.add(`hidden-elm`);
-    newsCounter = 0;
-    newsAPI().then(news => {
-      CreatCardNews(news);
-    });
-  } else if (currentElemNumber.textContent === `2`) {
-    const ellipsisSecondx2 = document
-      .querySelector(`.pagination-ellipsis_item2`)
-      .classList.add(`hidden-elm`);
-    newsCounter = 8;
-    newsAPI().then(news => {
-      CreatCardNews(news);
-    });
-  } else if (currentElemNumber.textContent === `3`) {
-    const ellipsisSecondx2 = document.querySelector(`.pagination-ellipsis_item2`).classList.add(`hidden-elm`);
-    nextPagex2.disabled = true
-  newsCounter = 16;
-     newsAPI()
-    .then(news => {
-    CreatCardNews(news);
-  })
-  }
-});
+//   if (currentElemNumber.textContent === `1`) {
+//     const ellipsisSecondx2 = document
+//       .querySelector(`.pagination-ellipsis_item2`)
+//       .classList.add(`hidden-elm`);
+//     newsCounter = 0;
+//     newsAPI().then(news => {
+//       CreatCardNews(news);
+//     });
+//   } else if (currentElemNumber.textContent === `2`) {
+//     const ellipsisSecondx2 = document
+//       .querySelector(`.pagination-ellipsis_item2`)
+//       .classList.add(`hidden-elm`);
+//     newsCounter = 8;
+//     newsAPI().then(news => {
+//       CreatCardNews(news);
+//     });
+//   } else if (currentElemNumber.textContent === `3`) {
+//     const ellipsisSecondx2 = document.querySelector(`.pagination-ellipsis_item2`).classList.add(`hidden-elm`);
+//     nextPagex2.disabled = true
+//   newsCounter = 16;
+//      newsAPI()
+//     .then(news => {
+//     CreatCardNews(news);
+//   })
+//   }
+// });
 
-function CreatCardNews(news) {
-  const markupArray = news.map(news => {
-    let words = news.content.split(' ');
-    if (words.length > 30) {
-      let shordDesc = words.slice(0, 30).join(' ') + '...';
+// // function CreatCardNews(news) {
+// //   const markupArray = news.map(news => {
+// //     let words = news.content.split(' ');
+// //     if (words.length > 30) {
+// //       let shordDesc = words.slice(0, 30).join(' ') + '...';
 
-      let formattedDate = news.publishedAt.toString().slice(0, 10);
-      let replaceDat = formattedDate.replace(`-`, '/').replace(`-`, '/');
-      return `<li class="news-item">
-          <div class="news-thumb">
-            <img
-              class="img-news"
-              src="${news.urlToImage}"
-              alt="${news.description}"
-              width="395"
-              height="395"
-            />
-            <p class="filter-descr">${nameSearchAPI}</p>
-            <a href="#" class="link-add"
-              >Add to favorite
-              <svg class="add-icon" width="16" heigth="16">
-                <use href="${svg}#heart-filled"></use>
-              </svg>
-            </a>
-          </div>
-          <div class="desr">
-            <h2 class="title">
-              ${news.title}
-            </h2>
-            <p class="subtitle">
-              ${shordDesc}
-            </p>
-            <div class="other-line">
-              <p class="date">${replaceDat}</p>
-              <p class="hyperlink"><a href="${news.url}">Read more</a></p>
-            </div>
-          </div>
-        </li>`;
-    }
-  });
-  sectionCard.insertAdjacentHTML('beforeend', markupArray.join(''));
-  sectionCard.insertAdjacentHTML('afterbegin', weather());
+// //       let formattedDate = news.publishedAt.toString().slice(0, 10);
+// //       let replaceDat = formattedDate.replace(`-`, '/').replace(`-`, '/');
+// //       return `<li class="news-item">
+// //           <div class="news-thumb">
+// //             <img
+// //               class="img-news"
+// //               src="${news.urlToImage}"
+// //               alt="${news.description}"
+// //               width="395"
+// //               height="395"
+// //             />
+// //             <p class="filter-descr">${nameSearchAPI}</p>
+// //             <a href="#" class="link-add"
+// //               >Add to favorite
+// //               <svg class="add-icon" width="16" heigth="16">
+// //                 <use href="${svg}#heart-filled"></use>
+// //               </svg>
+// //             </a>
+// //           </div>
+// //           <div class="desr">
+// //             <h2 class="title">
+// //               ${news.title}
+// //             </h2>
+// //             <p class="subtitle">
+// //               ${shordDesc}
+// //             </p>
+// //             <div class="other-line">
+// //               <p class="date">${replaceDat}</p>
+// //               <p class="hyperlink"><a href="${news.url}">Read more</a></p>
+// //             </div>
+// //           </div>
+// //         </li>`;
+// //     }
+// //   });
+// //   sectionCard.insertAdjacentHTML('beforeend', markupArray.join(''));
+// //   sectionCard.insertAdjacentHTML('afterbegin', weather());
 
-}
+// // }
 
-currentTurgetPage;
-ListenerNumberPage.addEventListener(`click`, e => {
-  console.log(numberPage);
-  if (currentTurgetPage.textContent === e.target.textContent) {
-    return;
-  }
-  // if (e.target.nodeName !== `A` || e.target.nodeName !== `BUTTON`) {
-  //     return
-  // }
+// // currentTurgetPage;
+// // ListenerNumberPage.addEventListener(`click`, e => {
+// //   console.log(numberPage);
+// //   if (currentTurgetPage.textContent === e.target.textContent) {
+// //     return;
+// //   }
+//   // if (e.target.nodeName !== `A` || e.target.nodeName !== `BUTTON`) {
+//   //     return
+//   // }
 
-  newsAPI()
-    .then(news => {
-      CreatCardNews(news.data.articles);
-      console.log(news);
-    })
-    .catch(err => console.log(err));
-});
+// //   newsAPI()
+// //     .then(news => {
+// //       CreatCardNews(news.data.articles);
+// //       console.log(news);
+// //     })
+// //     .catch(err => console.log(err));
+// // });
 
 filterButton.addEventListener(`click`, e => {
   if (e.target.nodeName !== `A`) {
@@ -218,25 +218,25 @@ filterButton.addEventListener(`click`, e => {
     .catch(err => console.log(err));
 });
 
-filterButton.addEventListener(`click`, e => {
-  if (e.target.nodeName !== `A`) {
-    return;
-  }
-  nameSearchAPI = e.target.textContent.trim();
-  console.log(nameSearchAPI);
+// // filterButton.addEventListener(`click`, e => {
+// //   if (e.target.nodeName !== `A`) {
+// //     return;
+// //   }
+// //   nameSearchAPI = e.target.textContent.trim();
+// //   console.log(nameSearchAPI);
 
-  // clearCard()
-  //     newsAPI()
-  // .then(news => {
-  //   CreatCardNews(news.data.articles);
-  //   console.log(news);
-  // })
-  // .catch(err => console.log(err))
-});
+//   // clearCard()
+//   //     newsAPI()
+//   // .then(news => {
+//   //   CreatCardNews(news.data.articles);
+//   //   console.log(news);
+//   // })
+//   // .catch(err => console.log(err))
+// // });
 
-function clearCard() {
-  sectionCard.innerHTML = ``;
-}
+// function clearCard() {
+//   sectionCard.innerHTML = ``;
+// }
 
 
 function weather() {
