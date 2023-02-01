81aafbe702e0037e25181b58534644cb2bfd13a6
iamblashch <113548705+iamblashch@users.noreply.github.com>
2023-02-01 21:30:02 +0000

    Deploying to gh-pages from @ iamblashch/goit-team-project-bandido-js@27c9504a0fd890898dae8ee164cd9ddcbda18f27 🚀

--- favourite.16522c4d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".news-list-fav");console.log(e),function(){const o=localStorage.getItem("markup-news-card");console.log(o),e.innerHTML=o}(),e.addEventListener("click",(function(e){if("BUTTON"!==e.target.nodeName)return;const o=e.target;console.log(o),o.classList.contains("add-to-fav")&&localStorage.removeItem("markup-news-card")}));
-//# sourceMappingURL=favourite.16522c4d.js.map
